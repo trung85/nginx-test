81bf2661a4aa68c484673d811476b8cb65c4f981
baby09211 <baby092111@gmail.com>
2018-10-22 13:48:17 +0700

    add file to deploy circleci

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SERVICE_NAME=cuongtv-service
+CLUSTER_NAME=cuongtv-clus
+BUILD_NUMBER=${CIRCLE_BUILD_NUM}
+IMAGE_TAG=${CIRCLE_SHA1}
+TASK_FAMILY=cuongtv-fargate
+
+# Create a new task definition for this build
+sed -e "s;%IMAGE_TAG%;${IMAGE_TAG};g" fargate-task.json > cuongtv-${BUILD_NUMBER}.json
+aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://cuongtv-${BUILD_NUMBER}.json
+
+# Update the service with the new task definition and desired count
+TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+DESIRED_COUNT=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | head -1 | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+if [ ${DESIRED_COUNT} = "0" ]; then
+    DESIRED_COUNT="1"
+fi
+
+aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
